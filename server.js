ce6b046f4fbcf6398ad905c55924e0e32ebd4708
Veronika Adelson <adelson1606@gmail.com>
2020-05-30 18:31:26 +0300

    heroku

--- server.js
+++ server.js
@@ -8,7 +8,5 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
-const port = 3001
-app.listen(port, () => {
-  console.log(`GoldenRush running on ${port} `)
-})
+const PORT = 8080
+app.listen(process.env.PORT || PORT)
